3afae578f449d6998193fb6db574c6cf58e98dd2
suk-6 <wsuk.nam@gmail.com>
2022-07-11 19:56:17 +0900

    220711

--- make_barcode.py
+++ make_barcode.py
@@ -5,11 +5,36 @@ from microbit import switch
 now = datetime.now()
 
 xlsx = pandas.read_excel("./data.xlsx", names=['ID','NAME','BARCODE'], engine='openpyxl')
-
 barcode_list = xlsx['BARCODE'].values.tolist()
 
 input_code = ""
+input_command = ""
 
+sec = True
+
+#command
+add_ID = ""
+add_NAME = ""
+add_BARCODE = ""
+
+#관리자 암호 불러오기
+admin_file = open("../Barcode_2_private/admin_key.txt", "r");
+admin_key = admin_file.read()
+admin_file.close()
+
+def reload():
+    xlsx = pandas.read_excel("./data.xlsx", names=['ID','NAME','BARCODE'], engine='openpyxl')
+    barcode_list = xlsx['BARCODE'].values.tolist()
+    print("reload Success")
+
+def user_add(add_ID, add_NAME, add_BARCODE):
+    out = xlsx.values.tolist()
+    out.append([add_ID, add_NAME, add_BARCODE])
+    df = pandas.DataFrame(out, columns=['ID', 'NAME', 'BARCODE'])
+    df.to_excel('./data.xlsx', index=False)
+    
+reload()
+    
 while True:
     input_code = input("code: ")
     if input_code in barcode_list:
@@ -20,9 +45,47 @@ while True:
         print("출입 허가 : ", export)
         print("출입 시각 : ", time_now)
         switch(1)
+    elif input_code == admin_key:
+        while True:
+            input_command = input("command: ")
+            if input_command == "add":
+                add_ID = input("ID: ")
+                add_NAME = input("NAME: ")
+                add_BARCODE = input("BARCODE: ")
+                user_add(add_ID, add_NAME, add_BARCODE)
+                reload()
+                print("Success!")
+            elif input_command == "open":
+                switch(1)
+                time_now = now.strftime('%Y-%m-%d %H:%M:%S')
+                print("관리자 출입")
+                print("출입 시각 : ", time_now)
+                break
+            elif input_command == "sec":
+                if sec is True:
+                    sec = False
+                    print("보안 해제")
+                elif sec is False:
+                    sec = True
+                    print("보안 중")
+                break
+            elif input_command == "reload":
+                reload()
+                print("User data Reload")
+                break
+            elif input_command == "exit":
+                print("Logout")
+                break
     elif input_code == "exit":
         print("Program Exit")
         break
     else:
-        print("등록되지 않은 바코드 : ", input_code)
-    
+        if sec is False:
+            switch(1)
+            time_now = now.strftime('%Y-%m-%d %H:%M:%S')
+            print("허가되지 않은 사용자 출입")
+            print("출입 시각 : ", time_now)
+        elif sec is True:
+            time_now = now.strftime('%Y-%m-%d %H:%M:%S')
+            print("등록되지 않은 바코드 : ", input_code)
+            print("인식 시각 : ", time_now)
